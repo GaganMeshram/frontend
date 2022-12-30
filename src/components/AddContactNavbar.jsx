5ce33716a34f623b5f005d7888dba0f618104851
Gagan <gmeshram774@gmail.com>
2022-12-31 01:16:25 +0530

    modified navbars

--- src/components/AddContactNavbar.jsx
+++ src/components/AddContactNavbar.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 function AddContactNavbar() {
   return (
-    <div className="row text-center p-3 bg-light">
+    <div className="row p-2 align-baseline bg-light">
       {/* col 1 navbar */}
       <div className="bg-light text-center col-sm-12 mt-1 col-lg-4 col-md-4">
-        <h1 className="navbar-brand">Contacx</h1>
+        <h1 className="navbar-brand text-center">Contacx</h1>
       </div>
       {/* col2 Serch bar*/}
-      <div className="bg-success rounded-3 border-3 text-center col-sm-12 mt-1 col-lg-4 col-md-4">
-        <h1 className="brand">Add Contact</h1>
+      <div className="bg-success rounded-3 col-sm-12 mt-1 col-lg-4 col-md-4">
+        <h1 className="navbar-brand text-center my-2 mx-2">Add Contact</h1>
       </div>
 
       {/* col3 */}
-      <div className="bg-light text-center mt-1 col-sm-12 col-lg-4 col-md-4">
+      <div className="bg-light text-center mt-3 col-sm-12 col-lg-4 col-md-4">
         <Link to={`/`} className="btn btn-dark">
           Go Back
         </Link>
--- src/components/EditContactNavbar.jsx
+++ src/components/EditContactNavbar.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 function EditContactNavbar() {
   return (
-    <div className="row text-center p-3 bg-light">
+    <div className="row p-2 align-baseline bg-light">
       {/* col 1 navbar */}
       <div className="bg-light text-center col-sm-12 mt-1 col-lg-4 col-md-4">
         <h1 className="navbar-brand">Contacx</h1>
       </div>
       {/* col2 Serch bar*/}
       <div className="bg-warning rounded-3 border-3 text-center col-sm-12 mt-1 col-lg-4 col-md-4">
-        <h1 className="brand">Update Contact</h1>
+        <h1 className="navbar-brand text-center my-2 mx-2">Update Contact</h1>
       </div>
 
       {/* col3 */}
-      <div className="bg-light text-center mt-1 col-sm-12 col-lg-4 col-md-4">
+      <div className="bg-light text-center mt-3 col-sm-12 col-lg-4 col-md-4">
         <Link to={`/`} className="btn btn-dark">
           Go Back
         </Link>
